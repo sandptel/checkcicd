d7f48b7560bd8ae817e4dd3f32ac98cbc7b6167b
Sandeep Patel <96694484+sandptel@users.noreply.github.com>
2024-05-11 23:56:38 +0530

    Update default.nix

--- default.nix
+++ default.nix
@@ -3,11 +3,6 @@ let
   pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 {
-  regolith-powerd = pkgs.callPackage ./regolith-powerd.nix { };
-  regolith-inputd = pkgs.callPackage ./regolith-inputd.nix {};
-  regolith-displayd = pkgs.callPackage ./regolith-displayd.nix {};
   ilia = pkgs.callPackage ./ilia.nix {};
-  rofication = pkgs.callPackage ./rofication.nix {};
-  trawl = pkgs.callPackage ./trawl.nix {};
 }
 
